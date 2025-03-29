191993984a4c841cb23bc0a927c1cf8125fd79b0
Naoto Tsushima <shogi2nao28@gmail.com>
2025-03-29 11:23:11 +0900

    Modify main.tf

--- main.tf
+++ main.tf
@@ -27,24 +27,55 @@ data "aws_subnet" "staging" {
   }
 }
 
+# IAMロールの作成
+resource "aws_iam_role" "ssm_role" {
+  name = "ec2_ssm_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# SSMのポリシーをアタッチ
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# IAMインスタンスプロファイルの作成
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ec2_ssm_profile"
+  role = aws_iam_role.ssm_role.name
+}
+
 # EC2インスタンスの作成
-resource "aws_instance" "example" {
-  ami           = "ami-0599b6e53ca798bb2"  # Amazon Linux 2023 AMI
+resource "aws_instance" "notebook" {
+  ami           = "ami-0599b6e53ca798bb2" # Amazon Linux 2023 AMI
   instance_type = "t2.micro"
 
   subnet_id                   = data.aws_subnet.staging.id
-  vpc_security_group_ids      = [aws_security_group.example.id]
+  vpc_security_group_ids      = [aws_security_group.notebook.id]
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ssm_profile.name
 
   tags = {
-    Name = "example-instance"
+    Name = "staging-notebook-ec2"
   }
 }
 
 # セキュリティグループの作成
-resource "aws_security_group" "example" {
-  name        = "example-security-group"
-  description = "Security group for example EC2 instance"
+resource "aws_security_group" "notebook" {
+  name        = "notebook-security-group"
+  description = "Security group for notebook EC2 instance"
   vpc_id      = data.aws_vpc.staging.id
 
   ingress {
@@ -59,17 +90,72 @@ resource "aws_security_group" "example" {
     from_port   = 8888
     to_port     = 8888
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 本番環境では適切なIPレンジに制限することを推奨
+    cidr_blocks = ["0.0.0.0/0"] # 本番環境では適切なIPレンジに制限することを推奨
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "example-security-group"
+    Name = "notebook-security-group"
   }
-} 
+}
+
+# VPCエンドポイントの作成
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = data.aws_vpc.staging.id
+  service_name        = "com.amazonaws.ap-northeast-1.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [data.aws_subnet.staging.id]
+  security_group_ids  = [aws_security_group.vpce.id]
+  private_dns_enabled = true
+  tags = {
+    Name = "ssm-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id              = data.aws_vpc.staging.id
+  service_name        = "com.amazonaws.ap-northeast-1.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [data.aws_subnet.staging.id]
+  security_group_ids  = [aws_security_group.vpce.id]
+  private_dns_enabled = true
+  tags = {
+    Name = "ssmmessages-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id              = data.aws_vpc.staging.id
+  service_name        = "com.amazonaws.ap-northeast-1.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [data.aws_subnet.staging.id]
+  security_group_ids  = [aws_security_group.vpce.id]
+  private_dns_enabled = true
+  tags = {
+    Name = "ec2messages-vpc-endpoint"
+  }
+}
+
+# VPCエンドポイント用のセキュリティグループ
+resource "aws_security_group" "vpce" {
+  name        = "vpce-security-group"
+  description = "Security group for VPC Endpoints"
+  vpc_id      = data.aws_vpc.staging.id
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.notebook.id]
+  }
+
+  tags = {
+    Name = "vpce-security-group"
+  }
+}
